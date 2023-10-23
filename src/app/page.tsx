b8514c981f806760497fedf6a4bc735338166b5f
Lukas Gjetting <lukasgjetting@gmail.com>
2023-10-22 19:08:57 -0700

    Make mobile "friendly" :)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,10 +18,14 @@ export default function Home() {
             alt=""
             width={3088}
             height={2320}
-            className="h-screen object-cover object-left-top"
+            className="h-[75vh] sm:h-screen object-cover object-left-top"
           />
         </div>
-        <div className="w-[40vw] flex flex-col justify-between items-center">
+        <div className="flex lg:hidden text-lg sm:w-48 absolute bottom-4 right-4 left-4 sm:left-auto rounded bg-white/80 p-4">
+          Hejsa! Basserne anbefaler at du åbner siden på en tablet (på siden)
+          eller en computer.
+        </div>
+        <div className="hidden lg:flex min-w-[480px] w-[40vw] flex-col justify-between items-center">
           <div className="flex-1 flex flex-col items-center justify-center">
             <h1 className="text-6xl text-center">Velkommen til bassetur.dk</h1>
             <div className="h-16" />
